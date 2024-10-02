2782c2deaf12d8bfb43aa4a766e9d1964aa7c431
tama4195 <tamaki4195@gmail.com>
2024-10-02 15:16:45 +0000

    merge sortwq

--- src/ALDS1_5/merge_sort/do_test.sh
+++ src/ALDS1_5/merge_sort/do_test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+make
 cat testfile | ./main
--- src/ALDS1_5/merge_sort/main.cpp
+++ src/ALDS1_5/merge_sort/main.cpp
@@ -17,27 +17,31 @@ using namespace std;
 /*出力
  *
  */
-
 void merge(vector<uint64_t>& A, uint64_t left, uint64_t mid, uint64_t right) {
     uint64_t n1 = mid - left;
     uint64_t n2 = right - mid;
-    vector<uint64_t> L, R;
-    for (int i = 0; i < n1; i++) L[i] = A[left + i];
-    for (int i = 0; i < n2; i++) R[i] = A[mid + i];
-    L[n1] = INFINITY;
-    R[n2] = INFINITY;
+    vector<uint64_t> L(500000);
+    vector<uint64_t> R(500000);
+    for (uint64_t i = 0; i < n1; i++) L[i] = A[left + i];
+    for (uint64_t i = 0; i < n2; i++) R[i] = A[mid + i];
+    L[n1] = UINT64_MAX;
+    R[n2] = UINT64_MAX;
     uint64_t i, j;
     i = j = 0;
-    for (int k = left; k < right; k++) {
-        if (L[i] <= R[j])
-            A[k] = L[i++];
-        else
-            A[k] = R[j++];
+    for (uint64_t k = left; k < right; k++) {
+        if (L[i] <= R[j]) {
+            A[k] = L[i];
+			i++;
+		}
+        else {
+            A[k] = R[j];
+			j++;
+		}
     }
 }
 
 void mergeSort(vector<uint64_t>& A, uint64_t left, uint64_t right) {
-    if (left + 1 <= right) {
+    if (left + 1 < right) {
         uint64_t mid = (left + right) / 2;
         mergeSort(A, left, mid);
         mergeSort(A, mid, right);
@@ -47,16 +51,16 @@ void mergeSort(vector<uint64_t>& A, uint64_t left, uint64_t right) {
 }
 
 void printResult(vector<uint64_t>& A, uint64_t n) {
-    for (int i = 0; i < n - 1; i++) cout << A[i] << " ";
+    for (uint64_t i = 0; i < n - 1; i++) cout << A[i] << " ";
     cout << A[n - 1] << endl;
 }
 
 int main(void) {
     uint64_t n;
     cin >> n;
-    vector<uint64_t> A;
-    for (int i = 0; i < n; i++) cin >> A[i];
-    mergeSort(A, 0, n - 1);
+    vector<uint64_t> A(500001);
+    for (uint64_t i = 0; i < n; i++) cin >> A[i];
+    mergeSort(A, 0, n);
     printResult(A, n);
     return 0;
 }
