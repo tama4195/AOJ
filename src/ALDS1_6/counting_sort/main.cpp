23665c6e10d2819b422bc90ec9e74f13ebedea0d
tama4195 <tamaki4195@gmail.com>
2024-10-09 14:14:04 +0000

    counting sort

---
+++ src/ALDS1_6/counting_sort/main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*問題
+ * Counting Sort
+ * https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/6/ALDS1_6_A
+ */
+
+/*アルゴリズム
+ *
+ *
+ */
+
+/*出力
+ *
+ */
+
+#define SIZE (10001)
+uint64_t A[SIZE] = {0};
+uint64_t B[SIZE] = {0};
+uint64_t C[SIZE] = {0};
+
+int main(void) {
+	uint64_t n;
+	cin >> n;
+	for (uint64_t i=0;i<n;i++) cin >> A[i];
+    for (uint64_t i=0;i<n;i++) C[A[i]]++;
+    uint64_t index = 0;
+    for (uint64_t i=0;i<n;i++) {
+        if(C[i] != 0) {
+            for (uint64_t j=0; j<C[i]; j++) {
+                B[++index] = i;
+            }
+        }
+    }
+    for (uint64_t i=0; i<n-1; i++) cout << B[i] << " ";
+    cout << B[n-1] << endl;
+
+    return 0;
+}
+
