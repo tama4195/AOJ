595f4ba2ee308c2492e59a9767f1449b319eda0c
tama4195 <tamaki4195@gmail.com>
2024-10-11 03:14:03 +0000

    add partition

---
+++ src/ALDS1_6/partition/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*問題
+ * Partition
+ * https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/6/ALDS1_6_B
+ */
+
+/*
+ * 
+ *
+ */
+
+/*出力
+ *
+ */
+
+
+int main(void) {
+    return 0;
+}
