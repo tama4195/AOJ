229f70eb917cef588b535e5576c3a8a68321da77
tama4195 <tamaki4195@gmail.com>
2024-10-11 15:55:56 +0900

    add partition code

--- src/ALDS1_6/partition/main.cpp
+++ src/ALDS1_6/partition/main.cpp
@@ -10,7 +10,6 @@ using namespace std;
  */
 
 /*
- * 
  *
  */
 
@@ -18,7 +17,43 @@ using namespace std;
  *
  */
 
+const int SIZE = 100001;
+
+int partition(std::vector<int> &A, int p, int r) {
+    int x = A[r];
+    int i = p - 1;
+    for (int j = p; j < r; r++) {
+        if (A[j] <= x) {
+            i++;
+            int tmp = A[i];
+            A[i] = A[j];
+            A[j] = tmp;
+        }
+    }
+    A[i + 1] = A[r];
+    return i + 1;
+}
+
+void printResult(std::vector<int> &A, int n, int q) {
+	int i;
+    for (i = 0; i < n - 1; i++) {
+        if (i == q)
+            cout << "[" << A[i] << "]" << " ";
+        else
+            cout << A[i] << " ";
+    }
+    cout << A[i] << endl;
+}
 
 int main(void) {
+    int n;
+    cin >> n;
+    std::vector<int> A(SIZE);
+    for (int i = 0; i < n; i++) cin >> A[i];
+    int p = 0;
+    int r = n - 1;
+    int q = partition(A, p, r);
+    printResult(A, n, q);
+
     return 0;
 }
