f277831352087a9ccfeb09da02b1a6e0f75a69ba
tama4195 <tamaki4195@gmail.com>
2024-11-06 14:22:55 +0000

    abc122

--- src/AtCoder/abc122_b/count/main.cpp
+++ src/AtCoder/abc122_b/count/main.cpp
@@ -12,14 +12,21 @@ int main(void) {
 
     for (int i = 0; (size_t)i < s.size(); i++) {
         for (int j = i; (size_t)j < s.size(); j++) {
-            int cnt_a = std::count(s.begin() + i, s.begin() + j, 'A');
-            int cnt_c = std::count(s.begin() + i, s.begin() + j, 'C');
-            int cnt_g = std::count(s.begin() + i, s.begin() + j, 'G');
-            int cnt_t = std::count(s.begin() + i, s.begin() + j, 'T');
-            int sum = cnt_a + cnt_c + cnt_g + cnt_t;
-            if (sum == (abs(j - i))) {
-                if (max < sum) max = sum;
+            int sum = 0;
+            if (j == i) {
+                if (s[i] == 'A' or s[i] == 'C' or s[i] == 'G' or s[i] == 'T') {
+                    sum = 1;
+                }
+            } else {
+                int cnt_a = std::count(s.begin() + i, s.begin() + j, 'A');
+                int cnt_c = std::count(s.begin() + i, s.begin() + j, 'C');
+                int cnt_g = std::count(s.begin() + i, s.begin() + j, 'G');
+                int cnt_t = std::count(s.begin() + i, s.begin() + j, 'T');
+                sum = cnt_a + cnt_c + cnt_g + cnt_t;
             }
+            if (sum >= (abs(j - i))) {
+                if (max < sum) max = sum;
+            } else break;
         }
     }
 
